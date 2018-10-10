d5aeb8b3cca17de481edc6de8e2d43a78c0f8d9d
Ajver <zawlockidominik@gmail.com>
2018-10-10 18:08:09 +0200

    begin stuff

--- src/MainFiles/MainClass.java
+++ src/MainFiles/MainClass.java
@@ -1,4 +1,24 @@
 package MainFiles;
 
-public class MainClass {
+import jslEngine.*;
+
+import java.awt.*;
+
+public class MainClass extends jslEngine {
+
+    public MainClass() {
+        start("Gwóźdź programu", 700, 450);
+    }
+
+    protected void update(float et) {
+
+    }
+
+    protected void render(Graphics g) {
+
+    }
+
+    public static void main(String[] args) {
+        new MainClass();
+    }
 }
